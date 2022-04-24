7de7b0f3b26c2c30d0c852ed72c94c2bed4ff5c1
Matyas.Horky <62426727+Erest99@users.noreply.github.com>
2022-04-25 00:27:13 +0200

    first commit

---
+++ src/main/java/com/example/cv4/JavaTestConfig.java
@@ -0,0 +1,28 @@
+package com.example.cv4;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+@ComponentScan
+public class JavaTestConfig {
+
+    @Autowired
+    private myInterface mI;
+
+    @PostConstruct
+    public void test()
+    {
+        System.out.println("Loaded test enviroment");
+
+    }
+
+    public String getName() {
+        return "yes";
+    }
+}
---
+++ src/main/java/com/example/cv4/myInterface.java
@@ -0,0 +1,13 @@
+package com.example.cv4;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface myInterface {
+
+        @Bean
+        String metoda();
+
+
+}
